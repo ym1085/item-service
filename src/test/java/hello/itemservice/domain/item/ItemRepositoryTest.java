323bd447c4646db68764711fe505fd4efb00ae5c
ym1085 <youngmin1085@gmail.com>
2024-01-25 01:46:25 +0900

    feat: 상품 도메인 개발
    * 상품 엔티티 생성 후 상품 CRUD 기능 추가

---
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,70 @@
+package hello.itemservice.domain.item;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    /**
+     * 테스트 종료 후 데이터 제거
+     * @throws Exception
+     */
+    @AfterEach
+    void afterEach() throws Exception {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    public void save() throws Exception {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+
+        //when
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(savedItem.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        //given
+        Item item1 = new Item("itemA", 10000, 10);
+        Item item2 = new Item("itemB", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        //when
+        List<Item> result = itemRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+    }
+
+    @Test
+    public void updateItem() throws Exception {
+        //given
+        Item item = new Item("item1", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        //when
+        Item updateParam = new Item("item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        //then
+        Item findItem = itemRepository.findById(itemId);
+        assertThat(findItem.getItemName()).isEqualTo("item2");
+        assertThat(findItem.getPrice()).isEqualTo(20000);
+        assertThat(findItem.getQuantity()).isEqualTo(30);
+    }
+}
